19d6e7e00f5dcaac6038cc997297818265731fe7
Elana Simon <elanasimon95@gmail.com>
2024-01-09 14:55:16 -0800

    added in ability to specify n and set default to 15

--- hw1.py
+++ hw1.py
@@ -1,11 +1,15 @@
 import numpy as np
-def pascal(n = 10):
+
+
+def pascal(n: int = 15):
     for i in range(n):
-        for j in range(i+1):
-            print(np.math.factorial(i)/(np.math.factorial(j)*np.math.factorial(i-j)))
+        for j in range(i + 1):
+            print(
+                np.math.factorial(i) / (np.math.factorial(j) * np.math.factorial(i - j))
+            )
         print("  ")
-def main():
-    # print (pascal())
-    pascal()
+
+
 if __name__ == "__main__":
-    main()
+    n = int(input("Enter the number of rows: "))
+    pascal(n)
