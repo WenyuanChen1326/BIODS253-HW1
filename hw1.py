1e09a0a26a9d5cba590550e894d34b122623db03
Elana Simon <elanasimon95@gmail.com>
2024-01-09 15:50:56 -0800

    added runtime analysis

--- hw1.py
+++ hw1.py
@@ -1,4 +1,8 @@
+import sys
+import time
+
 import numpy as np
+import pandas as pd
 
 
 def pascal(n: int = 15):
@@ -10,6 +14,29 @@ def pascal(n: int = 15):
         print("  ")
 
 
+def runtime_analysis(n_repeats: int = 100, n_rows: int = 15):
+    timing_metrics = []
+    for n in range(n_rows):
+        for i in range(n_repeats):
+            start = time.time()
+            pascal(n)
+            end = time.time()
+            timing_metrics.append({"Number of rows": n, "Time (s)": end - start})
+
+    return pd.DataFrame(timing_metrics)
+
+
 if __name__ == "__main__":
-    n = int(input("Enter the number of rows: "))
-    pascal(n)
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    # Take in n_rows as command line argument
+    n_rows = int(sys.argv[1])
+
+    # Run pascal's triangle with input n_rows
+    pascal(n=n_rows)
+
+    # Do runtime analysis and plot
+    timing_results = runtime_analysis()
+    sns.lineplot(data=timing_results, x="Number of rows", y="Time (s)")
+    plt.show()
